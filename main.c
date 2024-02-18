b581bab9709118bd635e6a8df1a008dd89a12e41
Asianadian <danielchen.mc@gmail.com>
2024-02-18 15:29:56 -0500

    Separate meta commands from main
    Create one function to handle all meta commands

--- main.c
+++ main.c
@@ -15,6 +15,11 @@ typedef SSIZE_T ssize_t;
 
 //structs
 
+typedef enum {
+  META_COMMAND_SUCCESS,
+  META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
 typedef struct {
   char* buffer;
   size_t buffer_length;
@@ -56,6 +61,14 @@ void close_input_buffer(InputBuffer* input_buffer) {
     free(input_buffer);
 }
 
+MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
+  if (strcmp(input_buffer->buffer, ".exit") == 0) {
+    exit(EXIT_SUCCESS);
+  } else {
+    return META_COMMAND_UNRECOGNIZED_COMMAND;
+  }
+}
+
 int main(int argc, char* argv[]) {
   InputBuffer* input_buffer = new_input_buffer();
 
@@ -63,13 +76,16 @@ int main(int argc, char* argv[]) {
     print_prompt();
     read_input(input_buffer);
 
-    if (strcmp(input_buffer->buffer, ".exit") == 0) {
-      close_input_buffer(input_buffer);
-      exit(EXIT_SUCCESS);
+    if (input_buffer->buffer[0] == '.') {
+      switch(do_meta_command(input_buffer)) {
+        case (META_COMMAND_SUCCESS):
+          continue;
+        case (META_COMMAND_UNRECOGNIZED_COMMAND):
+          printf("Unrecognized command '%s'\n", input_buffer->buffer);
+          continue;
+      }
     }
-    else {
-      printf("Unrecognized command '%s'.\n", input_buffer->buffer);
-    }
+    //else
   }
 }
 
