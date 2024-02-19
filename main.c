23709000468159e9e28453978ab7c4f4e929d2ee
Asianadian <danielchen.mc@gmail.com>
2024-02-18 19:45:32 -0500

    Place buffer information into statement when prepared
    Add property to statements to store buffer information for insert

--- main.c
+++ main.c
@@ -13,6 +13,9 @@ typedef SSIZE_T ssize_t;
 #define true 1
 #define false 0
 
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+
 //structs
 
 typedef enum {
@@ -46,7 +49,14 @@ InputBuffer* new_input_buffer() {
 }
 
 typedef struct {
+  uint32_t id;
+  char username[COLUMN_USERNAME_SIZE];
+  char email[COLUMN_EMAIL_SIZE];
+} Row;
+
+typedef struct {
   StatementType type;
+  Row row_to_insert;
 } Statement;
 
 //methods
@@ -86,6 +96,13 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
   if (strncmp(input_buffer->buffer, "INSERT", 6) == 0) {
     statement->type = STATEMENT_INSERT;
+    int args_assigned = sscanf(
+      input_buffer->buffer, 
+      "insert %d %s %s", 
+      &(statement->row_to_insert.id),
+      statement->row_to_insert.username, 
+      statement->row_to_insert.email
+    );
     return PREPARE_SUCCESS;
   }
   if (strcmp(input_buffer->buffer, "SELECT") == 0) {
@@ -96,7 +113,7 @@ PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement)
   return PREPARE_UNRECOGNIZED_STATEMENT;
 }
 
-void execute_statement(InputBuffer* input_buffer, Statement statement) {
+void execute_statement(Statement statement) {
   switch (statement.type) {
     case (STATEMENT_INSERT):
       printf("This is where an insert is done\n");\
