8897929f972047f84df7acc2b07d78d24cdeda3f
Asianadian <danielchen.mc@gmail.com>
2024-02-21 21:31:33 -0500

    Added tests for strings at max length and past max length

--- tests/main_tests.rb
+++ tests/main_tests.rb
@@ -26,4 +26,45 @@ describe 'database' do
             "db > ",
         ])
     end
+
+    it 'prints error message when table is full' do
+        script = (1..1401).map do |i|
+            "INSERT #{i} user#{i} #{i}@gmail.com"
+        end
+        script << ".exit"
+        result = run_script(script)
+        expect(result[-2]).to eq('db > Table is full')
+    end
+
+    it 'inserts max length strings' do
+        long_user = "a"*32
+        long_email = "a"*255
+        result = run_script([
+            "INSERT 1 #{long_user} #{long_email}",
+            "SELECT",
+            ".exit",
+        ])
+        expect(result).to match_array([
+            "db > Execute success",
+            "db > (1, #{long_user}, #{long_email})",
+            "Execute success",
+            "db > ",
+        ])
+    end
+
+    it 'prints error message when string over max length' do
+        long_user = "a"*33
+        long_email = "a"*256
+        result = run_script([
+            "INSERT 1 #{long_user} #{long_email}",
+            "SELECT",
+            ".exit",
+        ])
+        expect(result).to match_array([
+            "db > Execute success",
+            "db > (1, #{long_user}, #{long_email})",
+            "Execute success",
+            "db > ",
+        ])
+    end
 end
